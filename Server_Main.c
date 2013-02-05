69a7804e657e81b17f77b8727619a553405fbab2
Adam Jirasek <adam.jirasek@gmail.com>
2013-02-05 15:04:40 -0700

    - getopt

--- Server_Main.c
+++ Server_Main.c
@@ -2,61 +2,95 @@
 #include "libm3l.h"
 #include <pthread.h>
 
-extern lmint_t optind;
-static lmint_t verbose_flag;
+// extern lmint_t optind;
+// static lmint_t verbose_flag;
 
-lmint_t main (int argc, char ** argv){
+lmint_t main (int argc, char **argv){
 /*
  * function is a main server function
  * it reads data in libm3l format defining
  * inter-process communication
- * spawns a child for each data set
+ * spawns a child for each data set.
  */
-lmint_t count;
-  
-      for (count = 1; count < argc; count++)
-	{
-	  printf("argv[%d] = %s\n", count, argv[count]);
-	}
-    
 
-// 	optind = 0;
-// 		while (1)
-// 		{
-// 			static struct option long_options[] =
-// 			{
-// 				{"port",     	no_argument,    	0, 'p'},  /* port number */
-// 				{0, 0, 0, 0}
-// 			}; 
-//  /*
-//   * getopt_long stores the option index here. 
-//   */
-//  			c = getopt_long (args_num, opt, "abc", long_options, &option_index);
-// /*
-//  * Detect the end of the options 
-//  */
-// 			if (c == -1)
-// 				break;
-// 
-// 			switch (c)
-// 			{
-// 				case 0:
-//  
-// 					if (long_options[option_index].flag != 0)
-// 						break;
-// 			
-// 					printf ("option %s", long_options[option_index].name);
-// 					if (optarg)
-// 						printf (" with arg %s", optarg);
-// 					printf ("\n");
-// 					break;
-// 
-// 				case 'i':
-// /*
-//  * ignore case
-//  */
-// 					opts.opt_i = 'i';
-// 			}
-// 		}
+	lmint_t c, portnum;
+	lmint_t digit_optind = 0;
+	lmchar_t *Filename=NULL;
+	
+	node_t *Gnode;
+/*
+ * get options 
+ */
+	while (1) {
+		lmint_t this_option_optind = optind ? optind : 1;
+		lmint_t option_index = 0;
+		static struct option long_options[] = {
+			{"port",    		required_argument, 	0, 'p' },
+			{"help",     		no_argument,       	0, 'h' },
+			{"input_file",     	required_argument,	0, 'i' },
+			{0,         0,                 0,  0 }
+		};
+
+		c = getopt_long(argc, argv, "hi:p:012?",long_options, &option_index);
+		if (c == -1)
+			break;
+
+		switch (c) {
+			case 0:
+				printf("option %s", long_options[option_index].name);
+			if (optarg)
+				printf(" with arg %s", optarg);
+				printf("\n");
+			break;
+
+			case '0':
+			case '1':
+			case '2':
+				if (digit_optind != 0 && digit_optind != this_option_optind)
+					printf("digits occur in two different argv-elements.\n");
+				digit_optind = this_option_optind;
+				printf("option %c\n", c);
+			break;
+			
+			case 'i':
+				printf("option i with value '%s'\n", optarg);
+				if ( (Filename = strdup(optarg)) == NULL)
+					Perror("strdup");
+			break;
+			
+			case 'p':
+				printf("option p with value '%s'\n", optarg);
+				portnum = atoi(optarg);
+			break;
+
+			case '?':
+			case 'h':
+				printf("Help : \n");
+			break;
+
+			default:
+				printf("?? getopt returned character code 0%o ??\n", c);
+		}
+	}
+
+	if (optind < argc) {
+		printf("non-option ARGV-elements: ");
+		while (optind < argc)
+			printf("%s ", argv[optind++]);
+		printf("\n");
+	}
+
+	if(Filename == NULL){
+		free(Filename);
+		Error("Server: no definition file specified\n");
+	}
+	if( (Gnode = m3l_Fread(Filename, (lmchar_t *)NULL))  == NULL){
+		free(Filename);
+		Error("Server: m3l_Fread");
+	}
+	
+	free(Filename);
+
+	exit(EXIT_SUCCESS);
 	
 }
