66c85428b0383cb97e266e77d5022f7073af90da
Adam Jirasek <Adam.Jirasek@gmail.com>
2013-03-10 12:55:28 -0600

    - passign ** instead of * to Identify_Data_Thread

--- Source/Identify_Data_Thread.h
+++ Source/Identify_Data_Thread.h
@@ -2,6 +2,6 @@
 #ifndef  __IDENTIFY_DATA_THREAD_H__
 #define  __IDENTIFY_DATA_THREAD_H__
 
-lmint_t Identify_Data_Thread(data_thread_str_t *);
+lmint_t Identify_Data_Thread(data_thread_str_t **);
 
 #endif
