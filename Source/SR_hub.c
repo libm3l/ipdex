3aa04dfb82a05b21c15793f9a09a9910ba198543
Adam Jirasek <Adam.Jirasek@gmail.com>
2013-07-16 15:51:38 -0600

    Added ATDT mode and KeepAliveMode

--- Source/SR_hub.c
+++ Source/SR_hub.c
@@ -9,6 +9,9 @@ void *SR_hub(void *arg)
 {
 
 	lmsize_t i;
+	node_t *List;
+	find_t *SFounds;
+	lmchar_t *ATDTMode, *KeepAllive_Mode;
 /*
  * function signals SR_Threads that all requests (one Sender and n_rec_proc Receivers) arrived and 
  * that it can start data transfer
@@ -22,12 +25,62 @@ void *SR_hub(void *arg)
  */
 	SR_hub_thread_str_t *c = (SR_hub_thread_str_t *)arg;
 
-		if(m3l_Cat(c->pList, "--all", "-L", "-P", "*",   (lmchar_t *)NULL) != 0)
-			Warning("CatData");
+// 		if(m3l_Cat(c->pList, "--all", "-L", "-P", "*",   (lmchar_t *)NULL) != 0)
+// 			Warning("CatData");
 /*
  * find AT-DT mode
  * find KEEP_CONN_ALIVE
  */
+		Pthread_mutex_lock(c->plock);
+		if( (SFounds = m3l_Locate(c->pList, "./Data_Set/CONNECTION/ATDT_Mode", "./*/*/*",  (lmchar_t *)NULL)) != NULL){
+			
+			if( m3l_get_Found_number(SFounds) != 1)
+				Error("SR_hub: Only one CONNECTION/ATDT_Mode per Data_Set allowed");
+/* 
+ * pointer to list of found nodes
+ */
+				if( (List = m3l_get_Found_node(SFounds, 0)) == NULL)
+					Error("Thread_Prt: NULL ATDT_Mode");
+			
+				ATDTMode = (lmchar_t *)m3l_get_data_pointer(List);
+/* 
+ * free memory allocated in m3l_Locate
+ */
+			m3l_DestroyFound(&SFounds);
+		}
+		else
+		{
+			Error("SR_hub: CONNECTION/ATDT_Mode not found\n");
+		}
+		
+		
+		if( (SFounds = m3l_Locate(c->pList, "./Data_Set/CONNECTION/KEEP_CONN_ALIVE_Mode", "./*/*/*",  (lmchar_t *)NULL)) != NULL){
+			
+			if( m3l_get_Found_number(SFounds) != 1)
+				Error("SR_hub: Only one CONNECTION/KEEP_CONN_ALIVE_Mode per Data_Set allowed");
+/* 
+ * pointer to list of found nodes
+ */
+				if( (List = m3l_get_Found_node(SFounds, 0)) == NULL)
+					Error("SR_hub: NULL KEEP_CONN_ALIVE_Mode");
+			
+				KeepAllive_Mode = (lmchar_t *)m3l_get_data_pointer(List);
+/* 
+ * free memory allocated in m3l_Locate
+ */
+			m3l_DestroyFound(&SFounds);
+		}
+		else
+		{
+			Error("SR_hub: CONNECTION/KEEP_CONN_ALIVE_Mode not found\n");
+		}
+		Pthread_mutex_unlock(c->plock);
+		
+		
+		
+		printf(" Modes are %c  %c \n", *ATDTMode, *KeepAllive_Mode);
+
+
 	while(1){
 /*
  * wait for semaphore from Thread_Prt that 
